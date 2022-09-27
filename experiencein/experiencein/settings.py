21938227b883bdab7fb12f985e74ec021b3c0dc8
Lucas Freitas <lucas.oliveira13@estudante.ifb.edu.br>
2022-09-27 16:00:00 -0300

    Diretório __pycahce_ removido; Aplicação perfis criada

--- experiencein/experiencein/settings.py
+++ experiencein/experiencein/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'perfis'
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
