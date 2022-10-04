741ac370aa9e4c07bb45062c6a202a966c396bf7
Lucas Freitas <lucas.oliveira13@estudante.ifb.edu.br>
2022-10-04 17:26:16 -0300

    Atualizando proejto

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from perfis.models import Perfil
 
 def index(request):
-    return HttpResponse('Bem-vindo ao Experiencein')
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
