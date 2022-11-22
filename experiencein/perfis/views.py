2ce93adfa0a12a3ca51fc66c13dad174080a7af8
Lucas Freitas <lucas.oliveira13@estudante.ifb.edu.br>
2022-11-22 17:47:36 -0300

    Aula 11

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
+
+@login_required
 def index(request):
-	return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
+    print(request.user.username) 
+    print(request.user.email)
+    print(request.user.user_permissions)
 
+    return render(request,'index.html',{'perfis': Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request) })
+
+@login_required
 def exibir(request, perfil_id):	
 	perfil = Perfil.objects.get(id=perfil_id)
 
@@ -12,16 +21,20 @@ def exibir(request, perfil_id):
 	
 	return render(request, 'perfil.html', {'perfil':perfil, "ja_e_contato":ja_e_contato})
 
+@permission_required('perfis.add_convite', raise_exception=True)
+@login_required
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(request)
 	perfil_logado.convidar(perfil_a_convidar)
 	return redirect('index')
 
+@login_required
 def aceitar(request, convite_id):
 	convite = Convite.objects.get(id=convite_id)
 	convite.aceitar()
 	return redirect('index')
 
+@login_required
 def get_perfil_logado(request):
-	return Perfil.objects.get(id=1)
+	return request.user.perfil
